a3ce614c006752f7ddc98282b7e8bf0d5df758d4
littlepinkturtle <jovienjovien@gmail.com>
2024-09-28 03:31:44 +0000

    1

--- app.py
+++ app.py
@@ -1,4 +1,10 @@
 from flask import Flask,render_template,request
+import google.generativeai as genai
+import os
+
+model = genai.GenerativeModel("gemini-1.5-flash")
+api = os.getenv("MAKERSUITE")
+genai.configure(api_key="AIzaSyBufya1kxpXArVpo0T5JX1aM6XBDSv9pLk")
 
 app = Flask(__name__)
 
@@ -16,5 +22,20 @@ def prediction_result_DBS():
     r = (-50.6 * q) + 90.2
     return(render_template("prediction_result_DBS.html",r=r))
 
-if __name__ =="__main__":
+@app.route("/faq",methods=["GET","POST"])
+def faq():
+    return(render_template("faq.html"))
+
+@app.route("/q1",methods=["GET","POST"])
+def q1():
+    r = model.generate_content("How should I diversify my investment portfolio?")
+    return(render_template("q1_reply.html",r=r))
+
+@app.route("/q2",methods=["GET","POST"])
+def q2():
+    q = request.form.get("q")
+    r = model.generate_content(q)
+    return(render_template("q2_reply.html",r=r))
+
+if __name__ == "__main__":
     app.run()
